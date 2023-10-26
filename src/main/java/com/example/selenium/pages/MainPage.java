d9c3763a023888e308a4cb64794f0ca93727bb67
Oleh Morozov <morozov89oleg@gmail.com>
2023-10-26 22:26:49 +0300

    Refactored to the Page Factory Pattern

--- src/main/java/com/example/selenium/pages/MainPage.java
+++ src/main/java/com/example/selenium/pages/MainPage.java
@@ -1,22 +1,32 @@
 package com.example.selenium.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class MainPage extends com.example.selenium.pages.BasePage {
+
+    @FindBy(css = "h2")
+    private WebElement title;
+
+    @FindBy(css = ".icon-2x")
+    private WebElement logout;
 
     // TODO define title and logout WebElements using @FindBy
 
     public MainPage(WebDriver driver) {
         super(driver);
+        PageFactory.initElements(driver, this);
     }
 
     public String getTitle() {
-        // TODO return title text
-        return null;
+        return title.getText();
     }
 
     public MainPage clickLogout() {
-        // TODO click logout and return MainPage
+        logout.click();
         return this;
     }
 }
